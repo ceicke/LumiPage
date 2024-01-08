ea3d376bd938ff5d39932236aca808c8addc2aba
Christoph Eicke <christoph.eicke@new-work.se>
2024-01-08 16:21:25 +0100

    Initial Commit

---
+++ LumiPage/LumiPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumiPageApp.swift
+//  LumiPage
+//
+//  Created by Christoph Eicke on 08.01.24.
+//
+
+import SwiftUI
+
+@main
+struct LumiPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
