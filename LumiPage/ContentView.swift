7f0e20f9a40d87fc8ef7806ea20e80e41d4f052a
Christoph Eicke <christoph@geisterstunde.org>
2024-01-09 02:13:32 +0100

    First working version.

--- LumiPage/ContentView.swift
+++ LumiPage/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Prevent the device from sleeping
+        UIApplication.shared.isIdleTimerDisabled = true
+        return true
+    }
+}
+
 struct ContentView: View {
+    // Define the list of colors
+    private let colors: [Color] = [.yellow, .red, .green, .blue]
+    
+    private let debug: Bool = false
+    
+    @AppStorage("currentColorIndex") private var currentColorIndex: Int = 0
+    @AppStorage("brightness") private var brightness: Double = 0.5
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            // Set the background color based on the current index
+            colors[currentColorIndex]
+                .edgesIgnoringSafeArea(.all)
+            Image(systemName: "book")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(Color.white)
+            Text("Enjoy.")
+                .foregroundStyle(Color.white)
+            if(debug) {
+                Text("\(String(format: "%.2f", brightness))")
+                    .foregroundStyle(Color.white)
+            }
         }
         .padding()
+        .background(colors[currentColorIndex])
+        .onTapGesture {
+            // Update the color index on tap
+            currentColorIndex = (currentColorIndex + 1) % colors.count
+        }
+        .gesture(
+           DragGesture()
+           .onChanged { value in
+               // Adjust brightness based on swipe direction
+               let delta = value.translation.height / 10000.0
+               brightness = max(0.0, min(1.0, brightness - delta))
+               UIScreen.main.brightness = brightness
+           }
+        )
+        .onAppear {
+            // Set the initial brightness and currentColorIndex value when the app appears
+            UIScreen.main.brightness = brightness
+            currentColorIndex = currentColorIndex
+        }
+        .onDisappear {
+            // Save the brightness and currentColorIndex values when the app disappears
+            UserDefaults.standard.set(brightness, forKey: "brightness")
+            UserDefaults.standard.set(currentColorIndex, forKey: "currentColorIndex")
+        }
+        
     }
 }
 
